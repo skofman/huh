60707a771517a5656fa862295b8ce620659bab39
Shlomo Kofman <skofman@indeed.com>
2020-04-01 15:02:52 -0700

    Login

--- client/src/components/Login.tsx
+++ client/src/components/Login.tsx
@@ -3,6 +3,7 @@ import { Paper, TextField, Button, Typography, Link } from '@material-ui/core';
 import TopBar from './TopBar';
 import { makeStyles } from '@material-ui/core/styles';
 import { useHistory } from 'react-router-dom';
+import { Formik, Form, FormikValues, FormikErrors } from 'formik';
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -40,38 +41,50 @@ const useStyles = makeStyles(() => ({
   }
 }));
 
+const validate = (values: FormikValues) => {
+  const errors: FormikErrors<FormikValues> = {};
+
+  const { username, password } = values;
+  if (!username) {
+    errors.username = 'Required';
+  }
+  if (!password) {
+    errors.password = 'Required';
+  }
+
+  return errors;
+};
+
 const Login: React.FunctionComponent = () => {
   const classes = useStyles();
   const history = useHistory();
 
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
   const [error, setError] = useState(false);
 
-  const login = useCallback(() => {
-    if (!username || !password) {
-      return;
-    }
+  const login = useCallback(
+    (values, actions) => {
+      const { username, password } = values;
 
-    fetch('/api/login', {
-      method: 'POST',
-      headers: { 'Content-Type': 'application/json' },
-      body: JSON.stringify({ username, password })
-    })
-      .then((res) => {
-        if (res.status !== 200) {
-          return setError(true);
-        }
-        setUsername('');
-        setPassword('');
-        setError(false);
-
-        history.push('/home');
+      fetch('/api/login', {
+        method: 'POST',
+        headers: { 'Content-Type': 'application/json' },
+        body: JSON.stringify({ username, password })
       })
-      .catch((e) => {
-        setError(true);
-      });
-  }, [username, password, history]);
+        .then((res) => {
+          if (res.status !== 200) {
+            return setError(true);
+          }
+          setError(false);
+          actions.resetForm();
+
+          history.push('/home');
+        })
+        .catch((e) => {
+          setError(true);
+        });
+    },
+    [history]
+  );
 
   return (
     <div className={classes.root}>
@@ -80,31 +93,49 @@ const Login: React.FunctionComponent = () => {
         Log in
       </Typography>
       <Paper elevation={3} className={classes.paper}>
-        <TextField
-          fullWidth
-          label="Username"
-          variant="outlined"
-          value={username}
-          onChange={(e) => setUsername(e.target.value)}
-          margin="dense"
-        />
-        <TextField
-          label="Password"
-          type="password"
-          variant="outlined"
-          value={password}
-          onChange={(e) => setPassword(e.target.value)}
-          margin="dense"
-          fullWidth
-        />
-        <Button fullWidth variant="contained" className={classes.btn} color="primary" onClick={login}>
-          Log in
-        </Button>
-        {error && (
-          <Typography variant="body2" className={classes.error}>
-            Error log in
-          </Typography>
-        )}
+        <Formik
+          validate={validate}
+          initialValues={{ username: '', password: '' }}
+          onSubmit={(values, actions) => login(values, actions)}
+          validateOnBlur={false}
+          validateOnChange={false}
+        >
+          {({ values, handleSubmit, handleChange, errors, touched }) => (
+            <Form onSubmit={handleSubmit}>
+              <TextField
+                fullWidth
+                label="Username"
+                variant="outlined"
+                value={values.username}
+                onChange={handleChange}
+                margin="dense"
+                name="username"
+                error={touched.username && !!errors.username}
+                helperText={errors.username}
+              />
+              <TextField
+                label="Password"
+                type="password"
+                variant="outlined"
+                value={values.password}
+                onChange={handleChange}
+                margin="dense"
+                fullWidth
+                name="password"
+                error={touched.password && !!errors.password}
+                helperText={errors.password}
+              />
+              <Button fullWidth variant="contained" color="primary" className={classes.btn} type="submit">
+                Log in
+              </Button>
+              {error && (
+                <Typography variant="body2" className={classes.error}>
+                  Error log in
+                </Typography>
+              )}
+            </Form>
+          )}
+        </Formik>
       </Paper>
       <Paper elevation={3} className={classes.paper}>
         <Typography variant="body2" className={classes.message}>
--- server/src/auth/login.ts
+++ server/src/auth/login.ts
@@ -1,8 +1,6 @@
 import { Request, Response } from 'express';
 
 const login = (req: Request, res: Response) => {
-  // console.log(req);
-
   return res.sendStatus(200);
 };
 
